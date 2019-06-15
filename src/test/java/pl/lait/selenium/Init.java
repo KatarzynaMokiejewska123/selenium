80f592fe5f8e1a125476ee71fa21b6360daeab96
KatarzynaMokiejewska123 <kmokiejewska@op.pl>
2019-06-15 11:14:33 +0200

    Testy nowe

--- src/test/java/pl/lait/selenium/Init.java
+++ src/test/java/pl/lait/selenium/Init.java
@@ -5,26 +5,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Init {
 
-	static WebDriver driver; 
-	
-	public static WebDriver getDriver() {
-		
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\kmoki\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        
-        driver=new ChromeDriver(); // w ramach obiektu wywołujemy metodę
-        
-        driver.get("http://newtours.demoaut.com"); 
-        return driver;
-   	}
-	
-	 public static void sleep(int seconds){
-		    try {
-				Thread.sleep(seconds*1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	static WebDriver driver;
+
+	public static WebDriver getDriver() { // mamy metode getDriver
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\kmoki\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		driver = new ChromeDriver(); // w ramach obiektu wywołujemy metodę
+
+		driver.get("http://newtours.demoaut.com");
+		return driver;
+	}
+
+	public static void sleep(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+	}
 	
+	public static void close() {
+		driver.quit();
+	}
 	
-	 }
 }
