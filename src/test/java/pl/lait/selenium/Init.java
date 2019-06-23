9c1107c28896eac8cac1492ed834c3b114330e4d
KatarzynaMokiejewska123 <kmokiejewska@op.pl>
2019-06-23 15:00:36 +0200

    Remote

--- src/test/java/pl/lait/selenium/Init.java
+++ src/test/java/pl/lait/selenium/Init.java
@@ -1,7 +1,12 @@
 package pl.lait.selenium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 
@@ -9,12 +14,26 @@ public class Init {
 
 	public static WebDriver getDriver() { // mamy metode getDriver
 
-		System.setProperty("webdriver.chrome.driver",
+		/*System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\kmoki\\Downloads\\chromedriver_win32\\chromedriver.exe");
-
-		driver = new ChromeDriver(); // w ramach obiektu wywołujemy metodę
-
+*/
+		//driver = new ChromeDriver(); // w ramach obiektu wywołujemy metodę
+		
+		URL url = null;
+				
+				try {
+					url=new URL("http://192.168.0.110:4444/wd/hub");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+	
+		driver = new RemoteWebDriver(url, capability);
+		
 		driver.get("http://newtours.demoaut.com");
+		
 		return driver;
 	}
 
